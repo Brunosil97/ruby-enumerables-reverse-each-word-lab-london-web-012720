b27beac7120ef9034c29ba759de6c7f1755a0fb6
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-12 16:58:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 
 def reverse_each_word(string)
-  array = string.split("")
+  array = string.split(" ")
   test_array = [] 
   array.collect do |string|
     test_array << array.reverse 
   end 
-  test_array.join("")
+  test_array.join(" ")
 end 
 reverse_each_word("Hello there, and how are you?")
 
