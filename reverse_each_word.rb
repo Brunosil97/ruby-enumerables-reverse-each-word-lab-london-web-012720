284d15c520d70f6fb00b046d1482f8764808347a
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-12 16:59:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,12 @@
 
 def reverse_each_word(string)
-  array = string.split(" ")
-  test_array = [] 
-  array.collect do |string|
-    test_array << string.reverse 
-  end 
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
   test_array.join(" ")
-end 
-reverse_each_word("Hello there, and how are you?")
+end
 
+
+reverse_each_word("Hello there, and how are you?")
