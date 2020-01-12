2545efec5eba2fad1b36b9ca78a2bd1a567a4c15
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-12 16:54:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,7 @@ def reverse_each_word(string)
   test_array = [] 
   array.collect do |string|
     test_array << array.reverse 
+  end 
+  test_array("")
+end 
+reverse_each_word("Hello there, and how are you?")
