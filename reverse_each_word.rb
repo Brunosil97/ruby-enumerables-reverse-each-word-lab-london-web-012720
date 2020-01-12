6e196d05675f370ac177d5c3250d2ac23bc6696d
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-12 16:59:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   test_array = [] 
   array.collect do |string|
-    test_array << array.reverse 
+    test_array << string.reverse 
   end 
   test_array.join(" ")
 end 
